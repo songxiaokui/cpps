013302f81ac108774980bdd6fe4970303a75f5b9
austsxk <austsxk@163.com>
2023-12-26 14:24:19 +0800

    62.using声明与using编译指令

--- memoryModelAndNamespace/src/memoryModelAndNamespace/namespace/namespace.cpp
+++ memoryModelAndNamespace/src/memoryModelAndNamespace/namespace/namespace.cpp
@@ -4,13 +4,37 @@
 
 #include "namespace.h"
 
+void fetch();
+void fetch()
+{
+    cout << "this is global fetch() calling" << endl;
+}
+
+
+char pail[] = "global pail";
+
+
+
+void usingNameSpace();
 void usingNameSpace()
 {
     Jill::fetch();
     Jack::fetch();
+    // 全局作用域函数调用
+    ::fetch();
 
     cout << "Jill:pail = " << Jill::pail << endl;
     cout << "Jack:pail = " << Jack::pail << endl;
+    cout << "Global::pail = " << ::pail << endl;
+
+}
+
+void localVariableSubstitution(void);
+void localVariableSubstitution(void)
+{
+    cout << "global variable pail is: " << pail << endl;  // is equal ::pail
+    using Jill::pail;
+    cout << "local variable substitution: pail is: " << pail << endl;
 }
 
 void Namespace(void)
@@ -28,4 +52,19 @@ void Namespace(void)
     // 全局名称空间：文件级别声明 全局变量就是全局名称空间
     // 使用名称空间的内容: 使用作用域解析运算符 ::
 
+
+    // using声明与using编译指令
+    // 目的: 简化对名称空间内名称的使用
+    // using声明: 声明就是使指定的标识符可用
+    // using编译指令: 使整个名称空间可用
+    localVariableSubstitution();
+
+    // using编译指令的使用:
+    // 使用 using namespace 名称空间;
+    using namespace Jill;
+    // cout << "Jill namespace pail: " << pail << endl;  // 产生二义性
+    // 与 ::pail 产生冲突 不知道是使用 Jill中的pail 还是使用全局中的pail
+    // 正确使用: 增加作用域解析运算符 指定明确的命名空间
+    cout << "Jill::pail: " << Jill::pail << endl;
+    cout << "Global::pail: " << ::pail << endl;
 }
